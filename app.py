aa4e5f0eb86699a837d0d06537cb858abeff252a
shiv pratap <spsinghrajawat49@gmail.com>
2025-06-03 10:08:27 +0530

    file upload code is added

--- app.py
+++ app.py
@@ -7,16 +7,20 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        filename = request.form['filename'].strip()
-        user_text = request.form['user_text']
 
+        filename = request.form['filename'].strip()
+
+        user_text = request.form['user_text']
+        file=request.files['file']
+        file.save(f"files/{file.filename}")
         # Ensure safe filename
-        if not filename.endswith('.txt'):
-            filename += '.txt'
+        if request.form['filename'].strip()!='' and request.form['user_text']!='':
+            if not filename.endswith('.txt'):
+                filename += '.txt'
 
         # Save to the file
-        with open(f"files/{filename}", 'w') as f:
-            f.write(user_text)
+            with open(f"files/{filename}", 'w') as f:
+                f.write(user_text)
 
         return f'Text saved successfully to {filename}!'
 
