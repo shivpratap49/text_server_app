2adc85bbb9b63ffb77346e8fb11663df392c3bf6
shivpratap49 <spsinghrajawat49@gmail.com>
2025-06-15 13:11:50 +0530

    files downoload add

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,send_file
 import os
 
 app = Flask(__name__)
 
+FILES_DIR = os.path.join(app.root_path, 'files')
 
+@app.route('/files')
+def list_files():
+    files = os.listdir(FILES_DIR)
+    return render_template('list_files.html', files=files)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -22,9 +27,16 @@ def index():
             with open(f"files/{filename}", 'w') as f:
                 f.write(user_text)
 
-        return f'Text saved successfully to {filename}!'
+        return f'File saved successfully to {filename}!'
 
     return render_template('index.html')
+@app.route('/download/<filename>')
+def download_file(filename):
+    file_path = os.path.join(FILES_DIR, filename)
+    if os.path.exists(file_path):
+        return send_file(file_path, as_attachment=True)
+    else:
+        return "File not found.", 404
 
 
 if __name__ == '__main__':
